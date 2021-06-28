41e25d84ba19fcbf899b84fe87b3771c074f076b
Alexandre Cândido Xavier <Alexandre.candido.xavier.ufes@gmail.com>
2021-06-28 11:24:22 -0300

    Update exemplo2.py

--- exemplos/exemplo2.py
+++ exemplos/exemplo2.py
@@ -22,7 +22,7 @@ path_var = '/home/alexandre/Dropbox/grade_2020/data/netcdf_files/'
 ds = xr.open_mfdataset(path_var + 'Tmax*.nc')
 
 # pegando a variavel Tmax entre 01/01/1980 a 31/12/2010
-Tmax_data = ds.Tmax.sel(time=slice('1961-01-01','1990-12-31'))
+Tmax_data = ds.Tmax.sel(time=slice('1961-01-01','1989-12-31'))
 
 # pegando os dados para o posicao de Umuarama/Parana
 Tmax_data_temporal = Tmax_data.sel(latitude=-23.76,longitude=-53.30,
