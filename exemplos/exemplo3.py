14d2d736b4009e28e7ce52ce12c9a13aaa16c1b3
Alexandre Cândido Xavier <Alexandre.candido.xavier.ufes@gmail.com>
2021-06-28 11:35:16 -0300

    Create exemplo3.py

---
+++ exemplos/exemplo3.py
@@ -0,0 +1,23 @@
+import xarray as xr
+import matplotlib.pyplot as plt
+
+"""Plotando media mensal da Umidade Relativa (1990-01-01-2019-12-31)
+RH_20010101_20200731_BR-DWGD_UFES_UTEXAS_v_X.XX.nc
+RH_19810101_20001231_BR-DWGD_UFES_UTEXAS_v_X.XX.nc
+RH_19610101_19801231_BR-DWGD_UFES_UTEXAS_v_X.XX.nc
+"""
+
+# pegando variavel
+path_var =  '/home/alexandre/Dropbox/grade_2020/data/netcdf_files/'
+ds = xr.open_mfdataset(path_var + 'RH*.nc')
+
+# pegando a variavel RH entre 01/01/2006-31/12/2016
+RH_data = ds.RH.sel(time=slice('1990-01-01', '2019-12-31'))
+
+# agrupando em media mensal
+RH_mean_month = RH_data.groupby('time.month').mean('time')
+
+# plotando
+RH_mean_month.plot(x='longitude', y='latitude', col='month',
+                   cmap='RdBu', col_wrap=4)
+plt.show(block=False)
