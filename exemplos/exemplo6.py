721a629e6bd472e96fa40e665fad4a879e9a5279
Alexandre Cândido Xavier <Alexandre.candido.xavier.ufes@gmail.com>
2021-06-28 12:53:57 -0300

    Add files via upload

---
+++ exemplos/exemplo6.py
@@ -0,0 +1,43 @@
+import xarray as xr # versao '0.9.6'
+import matplotlib.pyplot as plt # versao '2.0.2'
+
+""" Para todas as variaveis existem dois controles, um he a distancia 
+do centro da celula a estacao mais proxima ("dist_nearest") e o outro
+he o numero de estacoes/pluviometros dentro da celula (informacoes 
+ver "paper"). 
+Aqui serao plotados os controles da grade precipitacaoo em duas 
+localidades, Sorriso-MT e Campinas-SP (na verdade he da 
+celula mais proxima a estas cidades).
+"""
+
+# lendo arquivo
+path_var = '/home/alexandre/Dropbox/grade_2020/data/netcdf_files/Controls/'
+ds = xr.open_mfdataset(path_var + 'pr_Control*.nc')
+dist_nearest = ds['dist_nearest']
+count = ds['count']
+
+# Nome dos pontos
+Names = ['Sorriso-MT', 'Campinas-SP']
+lat_lon = [[-12.5, -55.7],
+           [-22.8, -47.0],]
+
+# plotando distancia do pluviometro mais proximo, ao longo do tempo,
+# que foi utilizado na interpolacao
+_, (ax1, ax2) = plt.subplots(2, 1)
+for n, names in enumerate(Names):
+    dist_nearest.sel(latitude=lat_lon[n][0],
+                     longitude=lat_lon[n][1],
+                     method='nearest').plot(ax=ax1, label=names)
+
+    # número de estações que contem na célula
+    count.sel(latitude=lat_lon[n][0],
+              longitude=lat_lon[n][1],
+              method='nearest').plot(ax=ax2, label=names)
+
+ax1.set_ylim(0, 400)
+ax1.legend()
+ax1.set_title('')
+ax2.set_ylim(-1, 6)
+ax2.legend()
+ax2.set_title('')
+plt.show(block=False)
