1d80b51c54a18399e3298591f6870e1d1b0bc84f
AlexandreCandidoXavier <alexandre.candido.xavier.ufes@gmail.com>
2023-06-14 14:35:48 -0300

    correcoes

--- exemplos/exemplo2.py
+++ exemplos/exemplo2.py
@@ -14,8 +14,8 @@ Tmax_19610101_19801231_BR-DWGD_UFES_UTEXAS_v_X.XX.nc
 
 # versoes
 print(xr.__version__) # 2022.11.0
-print(np.__version__) # 1.23.4
-print(pd.__version__) # 1.5.1
+print(np.__version__) # 1.24.2
+print(pd.__version__) # 1.5.3
 
 # set correct path of the variables
 path_var = '/home/alexandre/Dropbox/grade_2020/data/netcdf_files/'
